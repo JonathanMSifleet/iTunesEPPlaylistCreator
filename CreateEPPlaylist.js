10f5b2594dae7d73d5e31ec622ebff156028ee4b
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-11-10 12:19:57 +0000

    Fix bugs

--- CreateEPPlaylist.js
+++ CreateEPPlaylist.js
@@ -10,32 +10,35 @@ var	mainLibrary = iTunesApp.LibraryPlaylist;
 
 var	tracks = mainLibrary.Tracks;
 var	numTracks = tracks.Count;
-var 	i;
+var i;
+var j;
 var	albumArray = new Array();
 var trackArray = new Array();
 
 EPPlaylist = iTunesApp.CreatePlaylist(PlaylistName);
 
-for (i = 1; i <= numTracks; i++) {
-  var	currTrack = tracks.Item(i);
-  var	album = currTrack.Album;
-
-  if ((album !== undefined) && (album !== "")) {
-
-    // if album is valid then:
-    if (albumArray[album] == undefined) {
-      // if valbum doesn't already exist
-      albumArray[album, 1] = new Array();
+for (i = 0; i <= numTracks; i++) {
+  var itunesAlbum = currTrack.Album
+  // if album is valid then:
+  if ((itunesAlbum !== undefined) && (itunesAlbum !== "")) {
+    // if album doesn't already exist in array create new album:
+    var albumObject = {album: itunesAlbum, numTracks: 0};
+    if (albumArray[albumObject] === undefined) {
+      var albumObject = {album: itunesAlbum, numTracks: 1};
+      albumArray.push(albumObject);
     } else {
-      albumArray[1] = albumArray[1]++;
+      albumArray[albumObject].numTracks = albumArray[albumObject].numTracks++;
     }
   }
 }
 
 var arraySize = albumArray.length;
 for (i = 1; i <= arraySize; i++) {
-  if(albumArray[1] > 7) {
-    var track = albumArray[i];
-    EPPlaylist.AddTrack(track);
+  if(albumArray[i].numTracks > 7) {
+    var trackArray = albumArray[i].album;
+    var numTracks = trackArray.length;
+    for(j = 1; j <= numTracks; j++) {
+      var	currTrack = trackArray[j]
+      EPPlaylist.AddTrack(track);
   }
 }
