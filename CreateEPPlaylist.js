214e6b2d22663c60b730dad1db8b2b3fc30b3b3c
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-11-10 13:26:12 +0000

    Update CreateEPPlaylist.js
    Will not match an album after the second album.
    Does not increment tracks

--- CreateEPPlaylist.js
+++ CreateEPPlaylist.js
@@ -4,41 +4,79 @@
 Script based from Otto - http://ottodestruct.com       */
 
 // put your playlist name here
-var PlaylistName = "EPs";
-var	iTunesApp = WScript.CreateObject("iTunes.Application");
-var	mainLibrary = iTunesApp.LibraryPlaylist;
+var iTunesApp = WScript.CreateObject("iTunes.Application");
+var mainLibrary = iTunesApp.LibraryPlaylist;
 
-var	tracks = mainLibrary.Tracks;
-var	numTracks = tracks.Count;
+var tracks = mainLibrary.Tracks;
+var numTracks = tracks.Count;
+var albumArray = new Array();
+var trackArray = new Array();
+var currTrack;
+var itunesAlbum;
+var albumName;
+var albumObject;
+var albumMatch;
 var i;
 var j;
-var	albumArray = new Array();
-var trackArray = new Array();
+var k;
+var l;
 
-EPPlaylist = iTunesApp.CreatePlaylist(PlaylistName);
+EPPlaylist = iTunesApp.CreatePlaylist("EPs");
 
-for (i = 0; i <= numTracks; i++) {
-  var itunesAlbum = currTrack.Album
+for (i = 1; i <= numTracks; i++) {
+  currTrack = tracks.Item(i);
+  itunesAlbum = currTrack.Album;
+
   // if album is valid then:
-  if ((itunesAlbum !== undefined) && (itunesAlbum !== "")) {
+  if ((itunesAlbum != undefined) && (itunesAlbum != "")) {
     // if album doesn't already exist in array create new album:
-    var albumObject = {album: itunesAlbum, numTracks: 0};
-    if (albumArray[albumObject] === undefined) {
-      var albumObject = {album: itunesAlbum, numTracks: 1};
-      albumArray.push(albumObject);
+
+     albumName = itunesAlbum.toString();
+     albumObject = {album: itunesAlbum, numTracks: 1};
+     albumMatch = false;
+
+    for (i = 1; i <= albumArray.length; i++) {
+      if(albumArray[i] !== undefined) {
+        if(albumArray[i].album === albumObject.album) {
+          albumMatch = true;
+        }
+      }
+    }
+
+    if(albumMatch === true) {
+      WScript.Echo("Matching album" + albumObject.album + albumObject.numTracks);
+      for( l = 1; l <= albumArray.length; l++) {
+        if(albumArray[l] !== undefined) {
+          if(albumArray[l].album === albumObject) {
+            albumArray[l].numTracks = albumArray[l].numTracks + 1;
+          }
+        }
+      }
     } else {
-      albumArray[albumObject].numTracks = albumArray[albumObject].numTracks++;
+      WScript.Echo("New album" + albumObject.album + albumObject.numTracks);
+      albumArray.push(albumObject);
     }
   }
 }
 
-var arraySize = albumArray.length;
-for (i = 1; i <= arraySize; i++) {
-  if(albumArray[i].numTracks > 7) {
-    var trackArray = albumArray[i].album;
-    var numTracks = trackArray.length;
-    for(j = 1; j <= numTracks; j++) {
-      var	currTrack = trackArray[j]
-      EPPlaylist.AddTrack(track);
+for( k = 1; k <=  albumArray.length; k++) {
+  try{
+    if(albumArray[k] != undefined) {
+      addToPlaylist(albumArray[k]);
+    }
+  } catch (e) {
+    WScript.Echo(e);
   }
+}
+
+var addToPlaylist = function (element) {
+ if(element.numTracks > 7) {
+     trackArray = element.album;
+     numTracks = trackArray.length;
+    for( j = 1; j <= numTracks; j++) {
+      currTrack = trackArray[j];
+      EPPlaylist.AddTrack(currTrack);
+    }
+  }
+  return;
 }
